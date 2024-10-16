967970fa2287e1b2ba063607cb2291e6bb0208fb
Murilo Henrique Lodi <132021915+MuriloLodi@users.noreply.github.com>
2024-10-16 11:47:43 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,32 +1,21 @@
 <?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $_POST['mensagem'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = htmlspecialchars($_POST['nome']);
+    $email = htmlspecialchars($_POST['email']);
+    $telefone = $_POST['telefone'];
+    $mensagem = htmlspecialchars($_POST['mensagem']);
 
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "murilohlodi@gmail.com";
-  $assunto = "Contato pelo Site";
+    $to = "murilohlodi@gmail.com";
+    $subject = "Novo contato de $nome";
+    $body = "Nome: $nome\nE-mail: $email\n\nMensagem:\n$mensagem";
+    $headers = "From: $email";
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
-
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+    if (mail($to, $subject, $body, $headers)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Falha ao enviar o e-mail.";
+    }
+} else {
+    echo "Método de requisição inválido.";
+}
 ?>
