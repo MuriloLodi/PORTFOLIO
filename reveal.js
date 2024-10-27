8c814cb6b614ca5a8b5a4838c4f0abcc7b7009dd
Murilo Henrique Lodi <132021915+MuriloLodi@users.noreply.github.com>
2024-10-26 23:54:54 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,113 @@
+window.revelar = ScrollReveal({reset:true})
+
+// topo do site
+
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500        
+    })
+    
+// title
+
+revelar.reveal('.title',
+    {
+        duration: 2000,
+        distance: '90px'
+})
+
+//
+
+revelar.reveal('.efeitos-especial1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 100
+})
+revelar.reveal('.efeitos-especial2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 200
+})
+revelar.reveal('.efeitos-especial3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 300
+})
+revelar.reveal('.efeitos-especial4',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 400
+})
+revelar.reveal('.efeitos-especial5',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+})
+revelar.reveal('.efeitos-especial6',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 600
+})
+revelar.reveal('.efeitos-especial7',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 700
+})
+revelar.reveal('.efeitos-especial8',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 800
+})
+
+//
+
+revelar.reveal('.efeito-img-sobre',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+})
+revelar.reveal('.efeito-txt-sobre',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 600,
+        origin: 'right'
+})
+
+//
+
+revelar.reveal('.port1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 200
+})
+revelar.reveal('.port2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 400
+})
+revelar.reveal('.port3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 600
+})
